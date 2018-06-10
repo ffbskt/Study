09678d7e94bf0754fe12cda4154d15a929c80b07
ffbskt <ffbskt@gmail.com>
2018-06-10 13:34:48 +0300

    edx competitive

--- Edx Comp Prog/1.cpp
+++ Edx Comp Prog/1.cpp
@@ -1,8 +1,53 @@
-#include <iostream>
+#include <fstream>
+#include <vector>
+
+void w1t4() {
+    std::ifstream inf("input.txt");
+    std::ofstream ouf("output.txt");
+    std::vector<int> t;
+    std::vector<int> p;
+    int n;
+    int vt, vp;
+    int mpt = 1e9;
+    int ts = 0;
+    int ps = 0;
+    inf >> n;
+    while( --n >= 0) {
+        inf >> vt >> vp;
+        if (vt >= vp) {
+            ts += vt;
+        } else {
+            ps += vp;
+        }
+        mpt = std::min(mpt, std::abs(vp - vt));
+    }
+    if (ts and ps) {
+        ouf << ts + ps;
+    } else {
+        ouf << ts + ps - mpt;
+    }
+
+}
+
+void w1t3() {
+    std::ifstream inf("input.txt");
+    std::ofstream ouf("output.txt");
+    int a0, a1, a2, k, n;
+    // int k = 0;
+    inf >> a0 >> a1 >> a2 >> n;
+    for (int i = 0; i < n; ++i) {
+        k = a1 + a2 - a0;
+        a0 = a1;
+        a1 = a2;
+        a2 = k;
+    }
+    ouf << a0;// << std::endl;
+}
 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    w1t4();
     return 0;
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Study
+Sunday, 10. June 2018 09:18AM 
+[Edx Comp Prog](https://courses.edx.org/courses/course-v1:ITMOx+I2CPx+1T2018/course/) - course for competitive programing
+[LeetCode](https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/) - already tryed and had git repo [here](https://github.com/ffbskt/algorithm_cpp) 
